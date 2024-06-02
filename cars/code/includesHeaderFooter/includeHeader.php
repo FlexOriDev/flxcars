cd934e5b7f31a82555c248ae79968acb2ad54760
FlexOriDev <louis.peron.prive@gmail.com>
2024-06-02 13:44:21 +0200

    [MVC-V0.0.1]Start of MVC scheduling

--- cars/code/includesHeaderFooter/includeHeader.php
+++ cars/code/includesHeaderFooter/includeHeader.php
@@ -27,43 +27,16 @@
         <?php include("../scripts/checkboxSCRIPT.js"); ?>
     </script>
 
-    <!-- Place the following <script> and <textarea> tags your HTML's <body> -->
-    <script>
-        tinymce.init({
-            selector: 'textarea.history',
-            plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
-            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-            tinycomments_mode: 'embedded',
-            tinycomments_author: 'Author name',
-            mergetags_list: [
-                { value: 'First.Name', title: 'First Name' },
-                { value: 'Email', title: 'Email' },
-            ],
-            ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
-        });
-    </script>
 
 </head>
 
 <!---HEADER---HEADER---HEADER---HEADER---HEADER---HEADER---HEADER---HEADER---HEADER---HEADER---HEADER---HEADER---HEADER---HEADER---HEADER---HEADER---HEADER--->
 <!-- Navbar-->
 <header class="header">
-
-    <div class="header-top-top" id="haut-haut">
-
-    </div>
-
-    <div class="header-top">
-
-
-    </div>
-
-    <div class="header-mid">
-
-    </div>
-
+    <div class="header-top-top" id="haut-haut"></div>
+    <div class="header-top"></div>
+    <div class="header-mid"></div>
     <div class="header-content">
-
         <nav class="nav_header">
             <ul>
                 <li class="deroulant"><a href="../pages/pageIndex.php">Accueil</a>
